261d1b58d58daed9fe6d03123cf8ed44fa133679
Alain <alain.goldman@gmail.com>
2018-03-14 18:02:46 -0400

    track firebae updates

--- src/Firetest.jsx
+++ src/Firetest.jsx
@@ -8,28 +8,47 @@ class Firetest extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+        data: ""
     }
+    this.getChange();
   }
 
-  someChange = () => {
-    // alert("something");
-    // let q = firebase.database().ref(web3.eth.accounts[0]);
-    // let referalInfo = {
-    //   "username": "dipshit3000"
-    // };
-    // q.set(referalInfo).then((f) => {
-    //     alert("success");
-    // }).catch((e) => {
-    //     alert("error");
-    // })
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  
+  setChange = () => {
+    let ref = firebase.database().ref("example");
+    let val = this.getRandomInt(10000);
+    let change = {
+      "someVal": val
+    };
+    ref.set(change).then((f) => {
+        // alert("success");
+    }).catch((e) => {
+        alert("error");
+    })
+  }
+
+  getChange = () => {
+    firebase.database().ref("example").on("child_changed", function(snapshot, prevChildKey) {
+        this.setState({data: snapshot.val()})
+    }, this)
+  }
+
+  populateChange = () => {
+    if(this.state.data) {
+        return <div> {this.state.data} </div>
+    }
   }
 
   render() {
     return (
     	<div className="imagebg bg--dark" >
             <h2> Welcome to firetest </h2>
-            <div className="redbutton redbutton" onClick={this.someChange}>update in firebase</div>
+            <div className="redbutton redbutton" onClick={this.setChange}>updates</div>
+            {this.state.data}
     	</div>
     );
   }
