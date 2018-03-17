b02cfd43c52f87c35adb23e02ae043ca078d4ca5
Alain <alain.goldman@gmail.com>
2018-03-16 21:33:09 -0400

    add move list

--- server.js
+++ server.js
@@ -21,17 +21,44 @@ app.get('/', function (req, res) {
 
 app.post('/firetest', function (req, res) {
 	console.log(req.body);
-	console.log("attempt push to firebase");
-	let ref = firebase.database().ref("example");
-    let val = Math.floor(Math.random() * Math.floor(1000));;
-    let change = {
-      "someVal": val
-    };
-    ref.set(change).then((f) => {
-		console.log("success!")
-    }).catch((e) => {
-		console.log("error!")
-    })
+  console.log("attempt push to firebase");
+  console.log(Moves[1]);
+	// let ref = firebase.database().ref("example");
+  //   let val = Math.floor(Math.random() * Math.floor(1000));;
+  //   let change = {
+  //     "someVal": val
+  //   };
+  //   ref.set(change).then((f) => {
+	// 	console.log("success!")
+  //   }).catch((e) => {
+	// 	console.log("error!")
+  //   })
 });
 
 app.listen(process.env.PORT || 8080);	
+
+
+
+var Moves = {
+  1 : {
+      isDamage: true, 
+      delay: 0,
+      duration: 0,
+      accuracy: 0.93,
+      effect: 0.5
+  },
+  2 : {
+      isDamage: false, 
+      delay: 0,
+      duration: 0,
+      accuracy: 0.93,
+      effect: 0.5
+  },
+  3 : {
+      isDamage: true, 
+      delay: 0,
+      duration: 0,
+      accuracy: 0.93,
+      effect: 0.5
+  }
+};
